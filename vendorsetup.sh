6003182f2e240e94de81672aa84f38d15ab73422
Drpto <dr.juniornoob@gmail.com>
2023-09-12 09:16:50 -0700

    stone: Add GCam Go as prebuilt Camera
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,11 @@
-git clone --depth 1 https://github.com/stone-devs/device_xiaomi_sm6375-common device/xiaomi/sm6375-common
+git clone --depth=1 https://github.com/stone-devs/device_xiaomi_sm6375-common device/xiaomi/sm6375-common
 
-git clone --depth 1 https://github.com/stone-devs/proprietary_vendor_xiaomi vendor/xiaomi
+git clone --depth=1 https://github.com/stone-devs/proprietary_vendor_xiaomi vendor/xiaomi
 
-git clone --depth 1 https://github.com/stone-devs/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+git clone --depth=1 https://github.com/stone-devs/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 
-git clone --depth 1 https://github.com/stone-devs/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/stone-devs/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
 
-git clone --depth 1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
+
+git clone --depth=1 https://github.com/Drpto/packages_apps_prebuilts.git packages/apps/prebuilts
