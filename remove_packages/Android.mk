83a3c96a332f1adb8956d0f250c14f2d0b576249
Drpto <dr.juniornoob@gmail.com>
2023-09-20 21:27:31 +0530

    stone: Add some apps nucked while building
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail Photos YouTube Videos
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback talkback Recorder
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail Photos YouTube Videos RecorderPrebuilt documentsui
+LOCAL_OVERRIDES_PACKAGES += TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback talkback recorder
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
